5f884008e23379a35d6a2df4e48866b6543967e6
Evgeniy <turkin-e.v@yandex.ru>
2022-03-29 23:03:13 +0400

    Not responsive

---
+++ dist/js/script.js
@@ -0,0 +1,24 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.querySelector('.hamburger');
+    const overlay = document.querySelector('.menu__overlay');
+    const main = document.querySelector('.menu__main');
+    const close = document.querySelector('.menu__close');
+    const menu = document.querySelector('.menu');
+    hamburger.onclick = function() {
+        overlay.classList.add('menu__overlay_active');
+        main.classList.add('menu__main_active');
+        menu.classList.add('menu_active');
+        menu.style.transitionDelay='0s';
+    };
+    close.onclick = function() {
+        overlay.classList.remove('menu__overlay_active');
+        main.classList.remove('menu__main_active');
+        menu.classList.remove('menu_active');
+        menu.style.transitionDelay='1s';
+    };
+    const percent = document.querySelectorAll('.skills__level-percent'),
+          lines = document.querySelectorAll('.skills__level-scale span');
+    percent.forEach( (item, i) => {
+        lines[i].style.width = item.innerHTML;
+    });
+});
